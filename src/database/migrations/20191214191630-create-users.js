6dad981474ba0793087365bb6dfac243fc70e0be
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-14 17:36:20 -0300

    User Loader

--- src/database/migrations/20191214191630-create-users.js
+++ src/database/migrations/20191214191630-create-users.js
@@ -4,7 +4,7 @@ module.exports = {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        autoInctrement: true,
+        autoIncrement: true,
         primaryKey: true,
       },
       name: {
