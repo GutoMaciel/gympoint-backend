414df4a5440fe0e515493918624527ce95aa4be4
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-15 11:16:53 -0300

    User Controller and Bcrypt

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,9 +18,9 @@ module.exports = {
   },
   rules: {
     "prettier/prettier": "error",
-    "class-method-use-this": "off",
+    "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
   },
 };
