0ccc36704cd72387fca217ffdbe36321fd06d82c
Gustavo Maciel dos Santos <gustavomaciel@Gustavos-MacBook-Pro.local>
2019-12-16 20:36:28 -0300

    feature: jwt auth

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '27c452b85ef01b32c82b1591ac4f0254',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 // routes.get('/', (req, res) => res.json({ message: 'Helloooo' }));
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
